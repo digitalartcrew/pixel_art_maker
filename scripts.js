51fd2e559f3703e47c6cbb9eee1e2903a4a17a8b
donte <digitalartcrew@gmail.com>
2015-08-19 17:47:01 -0700

    Color Changes

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ document.body.appendChild(container);
 		newBox.addEventListener("click", turnBlue);
 	}
 
+
 //Add Color Palette
 var colorPad = document.createElement("div");
 colorPad.id = "colorPad";
@@ -36,76 +37,171 @@ document.body.appendChild(colorPad);
 
 //
 var red = document.createElement("div");
+red.id = "redBox";
 red.style.height = "20px";
 red.style.width = "100px";
 red.style.float = "left";
 red.style.backgroundColor = "red";
 document.getElementById("colorPad").appendChild(red);
+redBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnRed);
 
-//Add Red Event Listener
-var redPaint = document.getElementById("colorPad").getElementsByTagName("div")[0];
-redPaint.addEventListener("click", turnRed);
+}
+});
+
 
 
 var orange = document.createElement("div");
+orange.id = "orangeBox";
 orange.style.height = "20px";
 orange.style.width = "100px";
 orange.style.float = "left";
 orange.style.backgroundColor = "orange";
 document.getElementById("colorPad").appendChild(orange);
+orangeBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnOrange);
+
+}
+});
 
 var yellow = document.createElement("div");
+yellow.id = "yellowBox";
 yellow.style.height = "20px";
 yellow.style.width = "100px";
 yellow.style.float = "left";
 yellow.style.backgroundColor = "yellow";
 document.getElementById("colorPad").appendChild(yellow);
+yellowBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnYellow);
+
+}
+});
 
 var green = document.createElement("div");
+green.id = "greenBox";
 green.style.height = "20px";
 green.style.width = "100px";
 green.style.float = "left";
 green.style.backgroundColor = "green";
 document.getElementById("colorPad").appendChild(green);
+greenBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnGreen);
+
+}
+});
 
 var blue = document.createElement("div");
+blue.id = "blueBox";
 blue.style.height = "20px";
 blue.style.width = "100px";
 blue.style.float = "left";
 blue.style.backgroundColor = "blue";
 document.getElementById("colorPad").appendChild(blue);
+blueBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnBlue);
+
+}
+});
 
 
 var purple = document.createElement("div");
+purple.id = "purpleBox";
 purple.style.height = "20px";
 purple.style.width = "100px";
 purple.style.float = "left";
 purple.style.backgroundColor = "purple";
 document.getElementById("colorPad").appendChild(purple);
+purpleBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnPurple);
+
+}
+});
 
 var grey = document.createElement("div");
+grey.id = "greyBox";
 grey.style.height = "20px";
 grey.style.width = "100px";
 grey.style.float = "left";
 grey.style.backgroundColor = "grey";
 document.getElementById("colorPad").appendChild(grey);
+greyBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnGrey);
+
+}
+});
 
 
 var brown = document.createElement("div");
+brown.id = "brownBox";
 brown.style.height = "20px";
 brown.style.width = "100px";
 brown.style.float = "left";
 brown.style.backgroundColor = "brown";
 document.getElementById("colorPad").appendChild(brown);
+brownBox.addEventListener("click", function(){
+	var divs = document.querySelectorAll("div");
+	for (var i=1; i<801; i++){
+	divs[i].removeEventListener("click");
+	divs[i].addEventListener("click", turnBrown);
+
+}
+});
 
 function turnBlue() {
 	this.style.backgroundColor = "blue";
 }
 
 function turnRed() {
-	newBox.removeEventListener();
-	newBox.addEventListener("click",this.style.backgroundColor ="red");
-	
+	this.style.backgroundColor = "red";
 }
+
+function turnYellow() {
+	this.style.backgroundColor = "yellow";
+}
+
+function turnGreen() {
+	this.style.backgroundColor = "green";
+}
+
+function turnPurple() {
+	this.style.backgroundColor = "purple";
+}
+
+function turnGrey() {
+	this.style.backgroundColor = "grey";
+}
+
+function turnBrown() {
+	this.style.backgroundColor = "brown";
+}
+
+function turnOrange() {
+	this.style.backgroundColor = "orange";
+}
+
+
+
+
 };
 
